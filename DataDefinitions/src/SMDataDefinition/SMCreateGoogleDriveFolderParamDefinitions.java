cf1091abc24e1b732ccee656b719bf30a2685a55
edwin <edwinmwaniki@odcdc.com>
2019-01-08 11:31:10 -0500

    Edwin testing conflict

--- DataDefinitions/src/SMDataDefinition/SMCreateGoogleDriveFolderParamDefinitions.java
+++ DataDefinitions/src/SMDataDefinition/SMCreateGoogleDriveFolderParamDefinitions.java
@@ -2,9 +2,10 @@ package SMDataDefinition;
 
 public class SMCreateGoogleDriveFolderParamDefinitions {
 	
-	//These are used to pass parameters to a Google Web App script that is able to create new folders in Google Drive
+	//These are used to pass parameters to a Google Web App script that is able to create new folders in Google Drive:
 	
 	//Parameter names:
+	
 	public static final String foldername = "foldername";
 	public static final String parentfolderid = "parentfolderid";
 	public static final String username = "username";

a5bf73233e2642aa1bae9cebc950a23e3bc17ab3
Tom ronayne <tjprona@gmail.com>
2019-02-16 18:29:38 -0500

    Updated program revision date

--- ServiceManager/src/smcontrolpanel/SMUpdateData.java
+++ ServiceManager/src/smcontrolpanel/SMUpdateData.java
@@ -20,7 +20,7 @@ public class SMUpdateData extends java.lang.Object{
 
 	private static final int m_CurrentDatabaseVersion = 1353;
 	private static final String m_sVersionNumber = "1.4";
-	private static final String m_sLastRevisionDate = "2/15/2019";
+	private static final String m_sLastRevisionDate = "2/16/2019";
 	private static final String m_sCopyright = "Copyright 2003-2019 AIRO Tech OMD, Inc.";
 
 	private String m_sErrorMessage;

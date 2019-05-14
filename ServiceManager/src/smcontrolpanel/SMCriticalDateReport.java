2bb8bb1dacf6294dce24cfd5143d571343946106
Ben Alden <benalden@odcdc.com>
2019-05-14 15:02:03 -0400

    "and number" for name displayed in other
    Sorry, I didn't see the and number part of the Trello Card...

--- ServiceManager/src/smcontrolpanel/SMCriticalDateReport.java
+++ ServiceManager/src/smcontrolpanel/SMCriticalDateReport.java
@@ -330,7 +330,7 @@ public class SMCriticalDateReport extends java.lang.Object{
 						
 						if( rs.getInt(SMTablecriticaldates.TableName + "." + SMTablecriticaldates.itype) == SMTablecriticaldates.PURCHASE_ORDER_RECORD_TYPE) {
 							out.println("<b>Name: </b> VENDOR  " + rs.getString((SMTableicpoheaders.TableName + "." + SMTableicpoheaders.svendorname).replace("`", "")).trim() + ""); 
-							out.println("<br><b>Number: </b>" + rs.getString((SMTableicpoheaders.TableName + "." + SMTableicpoheaders.svendor).replace("`", "")).trim() + ""); 
+							out.println("<br><b>Number: </b> VENDOR " + rs.getString((SMTableicpoheaders.TableName + "." + SMTableicpoheaders.svendor).replace("`", "")).trim() + ""); 
 							out.println("<br><b>Status: </b>" + SMTableicpoheaders.getStatusDescription(rs.getInt((SMTableicpoheaders.TableName + "." + SMTableicpoheaders.lstatus))) + ""); 
 							out.println("<br><b>Expected: </b>" + clsDateAndTimeConversions.resultsetDateStringToString(rs.getString(SMTableicpoheaders.datexpecteddate)) + ""); 
 							out.println("<br><b>PO Number: </b>" + "<A HREF=\"" 

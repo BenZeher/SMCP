f45a4f9a5929f39c5f4f1ff8d99449d68d971208
Ben Zeher <benzeher@odcdc.com>
2019-06-24 07:05:37 -0400

    Allow zooming

--- ServiceManager/src/smcontrolpanel/SMWorkOrderEdit.java
+++ ServiceManager/src/smcontrolpanel/SMWorkOrderEdit.java
@@ -2902,7 +2902,7 @@ public class SMWorkOrderEdit  extends HttpServlet {
 		s += "<TITLE>" + subtitle + "</TITLE>"
 		+ SMUtilities.faviconLink()
 		//This line should keep the font widths 'screen' wide:
-		+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0 maximum-scale=1\" />"
+		+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />"
 		+ "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>"
 		+ "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">"
 		+ "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">"

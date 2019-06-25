1b1fdf0cfca0436229207000fa0e3adbf5c0ece8
Ben Alden <benalden@odcdc.com>
2019-06-25 16:10:51 -0400

    Whoops..

--- ServiceManager/src/smcontrolpanel/SMCriticalDateReportGenerate.java
+++ ServiceManager/src/smcontrolpanel/SMCriticalDateReportGenerate.java
@@ -215,15 +215,15 @@ public class SMCriticalDateReportGenerate extends HttpServlet {
     	out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
 		   "Transitional//EN\">" +
 	       "<HTML>" +
-	       "<HEAD><TITLE>" + sReportTitle + " - " + sCompanyName + "</TITLE></HEAD>\n<BR>\n" + 
-		   "<BODY BGCOLOR=\"#FFFFFF\">\n" +
+	       "<HEAD><TITLE>" + sReportTitle + " - " + sCompanyName + "</TITLE></HEAD>\n<BR>" + 
+		   "<BODY BGCOLOR=\"#FFFFFF\">" +
 		   "<TABLE BORDER=0 WIDTH=100% BGCOLOR=\"" + sColor + "\">" +
-		   "<TR><TD ALIGN=LEFT WIDTH=45%><FONT SIZE=2>\n" 
+		   "<TR><TD ALIGN=LEFT WIDTH=45%><FONT SIZE=2>" 
 		   + USDateformatter.format((new Timestamp(System.currentTimeMillis()))) + " Printed by " + sUserFirstName + " " + sUserLastName
-		   + "</FONT></TD>\n<TD ALIGN=CENTER WIDTH=55%><FONT SIZE=2><B>" + sCompanyName + "</B></FONT></TD></TR>\n" +
-		   "<TR><TD VALIGN=BOTTOM COLSPAN=2><FONT SIZE=4 ><B>" + sReportTitle + "</B></FONT></TD></TR>\n" +
+		   + "</FONT></TD><TD ALIGN=CENTER WIDTH=55%><FONT SIZE=2><B>" + sCompanyName + "</B></FONT></TD></TR>" +
+		   "<TR><TD VALIGN=BOTTOM COLSPAN=2><FONT SIZE=4 ><B>" + sReportTitle + "</B></FONT></TD></TR>" +
 		   
-		   "<TR><TD COLSPAN=2><FONT SIZE=2>" + sCriteria + "</FONT></TD></TR>\n");
+		   "<TR><TD COLSPAN=2><FONT SIZE=2>" + sCriteria + "</FONT></TD></TR>");
 				   
 	   out.println("<TD><A HREF=\"" + SMUtilities.getURLLinkBase(getServletContext()) + "smcontrolpanel.SMUserLogin?" 
 				+ SMUtilities.SMCP_REQUEST_PARAM_DATABASE_ID + "=" + sDBID 

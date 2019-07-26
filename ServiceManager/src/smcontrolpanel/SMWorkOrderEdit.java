0d5b6535e4fbb8b8d42f600d5385710dc5ecce95
Ben <benzeher@odcdc.com>
2019-07-26 14:34:15 -0400

    update to mobile.css

--- ServiceManager/src/smcontrolpanel/SMWorkOrderEdit.java
+++ ServiceManager/src/smcontrolpanel/SMWorkOrderEdit.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import ConnectionPool.WebContextParameters;
 import SMClasses.SMOption;
 import SMClasses.SMOrderDetail;
 import SMClasses.SMOrderHeader;

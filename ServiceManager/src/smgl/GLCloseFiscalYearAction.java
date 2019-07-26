94f64781f0fdcd94074b0116e4cb2ac2ad6dec0d
Tom ronayne <tjprona@gmail.com>
2019-07-26 11:35:05 -0400

    Worked on GL Closing functions

--- ServiceManager/src/smgl/GLCloseFiscalYearAction.java
+++ ServiceManager/src/smgl/GLCloseFiscalYearAction.java
@@ -1,5 +1,106 @@
 package smgl;
 
-public class GLCloseFiscalYearAction {
+import java.io.IOException;
+import java.sql.Connection;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import smcontrolpanel.SMMasterEditAction;
+import smcontrolpanel.SMSystemFunctions;
+
+public class GLCloseFiscalYearAction extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		SMMasterEditAction smaction = new SMMasterEditAction(request, response);
+		try {
+			smaction.getCurrentSession().removeAttribute(GLCloseFiscalYearEdit.GL_CLOSING_SESSION_WARNING_OBJECT);
+		} catch (Exception e2) {
+			//If this attribute isn't in the session, just go on without disruption....
+		}
+		if (!smaction.processSession(getServletContext(), SMSystemFunctions.GLCloseFiscalYear)){return;}
+	    //Read the entry fields from the request object:
+	    String sFiscalYear = ServletUtilities.clsManageRequestParameters.get_Request_Parameter(GLCloseFiscalYearEdit.PARAM_FISCAL_YEAR_SELECTION, request);
+	    String sBatchDate = ServletUtilities.clsManageRequestParameters.get_Request_Parameter(GLCloseFiscalYearEdit.PARAM_BATCH_DATE, request);
+		
+		if (request.getParameter(GLPullIntoConsolidationSelect.CONFIRM_PROCESS) == null){
+			smaction.getCurrentSession().setAttribute(GLCloseFiscalYearEdit.GL_CLOSING_SESSION_WARNING_OBJECT, "You must check the 'Confirm' checkbox to continue.");
+			smaction.redirectAction(
+					"", 
+					"", 
+		    		""
+				);
+				return;
+		}
+		
+    	Connection conn = null;
+    	try {
+			conn = ServletUtilities.clsDatabaseFunctions.getConnectionWithException(
+				getServletContext(), 
+				smaction.getsDBID(), 
+				"MySQL", 
+				this.toString() + ".updateCompanies - user: " + smaction.getFullUserName()
+			);
+		} catch (Exception e1) {
+			smaction.getCurrentSession().setAttribute(GLCloseFiscalYearEdit.GL_CLOSING_SESSION_WARNING_OBJECT, e1.getMessage());
+			smaction.redirectAction(
+					"", 
+					"", 
+		    		""
+				);
+				return;
+		}
+    	
+    	GLTransactionBatch glbatch = new GLTransactionBatch("-1");
+    	glbatch.setlcreatedby(smaction.getUserID());
+    	glbatch.setllasteditedby(smaction.getUserID());
+    	glbatch.setsbatchdate(sBatchDate);
+    	glbatch.setsbatchdescription("Closing Journal Entries");
+
+    	GLTransactionBatchEntry glentry = new GLTransactionBatchEntry();
+    	glentry.setsdatdocdate(sBatchDate);
+    	glentry.setsdatentrydate(sBatchDate);
+    	glentry.setsentrydescription("Closing Entry");
+    	glentry.setsentrynumber("1");
+    	glentry.setsfiscalperiod("15");
+    	glentry.setsfiscalyear(sFiscalYear);
+    	glentry.setssourceledger(GLSourceLedgers.getSourceLedgerDescription(GLSourceLedgers.SOURCE_LEDGER_JOURNAL_ENTRY));
+
+    	//Now get the transaction batch lines:
+    	//TODO:
+    	
+    	
+    	try {
+
+		} catch (Exception e) {
+			ServletUtilities.clsDatabaseFunctions.freeConnection(getServletContext(), conn, "[1562875614]");
+			smaction.getCurrentSession().setAttribute(GLCloseFiscalYearEdit.GL_CLOSING_SESSION_WARNING_OBJECT, e.getMessage());
+			smaction.redirectAction(
+				"", 
+				"", 
+	    		""
+			);
+			return;
+		}
+		ServletUtilities.clsDatabaseFunctions.freeConnection(getServletContext(), conn, "[1562875615]");
+		smaction.redirectAction(
+			"", 
+			"Company with ID '" + "" + "' was successfully pulled into the consolidated company.",
+    		""
+		);
+		return;
+	}
+
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		doPost(request, response);
+	}
 }
--- ServiceManager/src/smgl/GLCloseFiscalYearEdit.java
+++ ServiceManager/src/smgl/GLCloseFiscalYearEdit.java
@@ -1,5 +1,171 @@
 package smgl;
 
-public class GLCloseFiscalYearEdit {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ConnectionPool.WebContextParameters;
+import SMDataDefinition.SMTableglfiscalperiods;
+import ServletUtilities.clsDatabaseFunctions;
+import smcontrolpanel.SMAuthenticate;
+import smcontrolpanel.SMSystemFunctions;
+import smcontrolpanel.SMUtilities;
+
+public class GLCloseFiscalYearEdit extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static final String CONFIRM_PROCESS = "ConfirmProcess";
+	public static String PARAM_FISCAL_YEAR_SELECTION = "FISCALYEARSELECTION";
+	public static String PARAM_BATCH_DATE = "BATCHDATE";
+	public static final String GL_CLOSING_SESSION_WARNING_OBJECT = "GLCLOSINGWARNING";
+	
+	public void doGet(HttpServletRequest request,
+				HttpServletResponse response)
+				throws ServletException, IOException {
+	    
+	    response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		if (!SMAuthenticate.authenticateSMCPCredentials(
+				request, 
+				response, 
+				getServletContext(), 
+				SMSystemFunctions.GLCloseFiscalYear))
+		{
+			return;
+		}
+
+	    //Get the session info:
+	    HttpSession CurrentSession = request.getSession(true);
+	    String sDBID = (String) CurrentSession.getAttribute(SMUtilities.SMCP_SESSION_PARAM_DATABASE_ID);
+	    String sUserID = (String) CurrentSession.getAttribute(SMUtilities.SMCP_SESSION_PARAM_USERID);
+	    String sUserFullName = (String)CurrentSession.getAttribute(SMUtilities.SMCP_SESSION_PARAM_USERFIRSTNAME) + " "
+	    				+ (String)CurrentSession.getAttribute(SMUtilities.SMCP_SESSION_PARAM_USERLASTNAME);
+	    String sCompanyName = (String) CurrentSession.getAttribute(SMUtilities.SMCP_SESSION_PARAM_COMPANYNAME);
+	    String title = "GL Close Fiscal Year";
+	    String subtitle = "";
+	    
+	    out.println(SMUtilities.SMCPTitleSubBGColor(title, subtitle, SMUtilities.getInitBackGroundColor(getServletContext(), sDBID), sCompanyName));
+		
+	    out.println(SMUtilities.getMasterStyleSheetLink());
+	    out.println(SMUtilities.getDatePickerIncludeString(getServletContext()));
+	    
+	    //String sWarning = "";
+	    String sWarning = (String)CurrentSession.getAttribute(GL_CLOSING_SESSION_WARNING_OBJECT);
+	    CurrentSession.removeAttribute(GL_CLOSING_SESSION_WARNING_OBJECT);
+		if (sWarning != null){
+			out.println("<B><FONT COLOR=\"RED\">WARNING: " + sWarning + "</FONT></B><BR>");
+		}
+	    
+		//Print a link to the first page after login:
+	    out.println("<A HREF=\"" + SMUtilities.getURLLinkBase(getServletContext()) + "smcontrolpanel.SMUserLogin?" 
+				+ SMUtilities.SMCP_REQUEST_PARAM_DATABASE_ID + "=" + sDBID 
+				+ "\">Return to user login</A><BR>");
+	    out.println("<A HREF=\"" + SMUtilities.getURLLinkBase(getServletContext()) + "smgl.GLMainMenu?" 
+				+ SMUtilities.SMCP_REQUEST_PARAM_DATABASE_ID + "=" + sDBID 
+				+ "\">Return to General Ledger Main Menu</A><BR>");
+	    out.println("<A HREF=\"" + WebContextParameters.getdocumentationpageURL(getServletContext()) 
+	    	+ "#" + Long.toString(SMSystemFunctions.GLCloseFiscalYear) 
+	    	+ "\">Summary</A><BR>");
+	    
+	    Connection conn = null;
+		try {
+			conn = ServletUtilities.clsDatabaseFunctions.getConnectionWithException(
+				getServletContext(), 
+				sDBID, 
+				"MySQL", 
+				this.toString() + ".doGet - user: " + sUserFullName + " [1564153159]"
+			);
+		} catch (Exception e2) {
+			out.println("<BR><B><FONT COLOR=RED>Error [1564153160] getting connection - " + e2.getMessage() + "</FONT></B><BR>");
+			out.println("</BODY></HTML>");
+			return;
+		}
+	    
+	    GLOptions glopt = new GLOptions();
+	    if (!glopt.load(conn)){
+	    	ServletUtilities.clsDatabaseFunctions.freeConnection(getServletContext(), conn, "[1564153162]");
+			out.println("<BR><B><FONT COLOR=RED>Error [1564153161] loading GL Options - " + glopt.getErrorMessageString() + "</FONT></B><BR>");
+			out.println("</BODY></HTML>");
+			return;
+	    }
+	    
+	    out.println("<BR>This function will automatically create journal entries for the elected fiscal year"
+	    		+ " to move current balances from all the income statement accounts into the single 'retained earnings'"
+	    		+ " account (GL account " + glopt.getsClosingAccount() + ")."
+	    		+ "<BR><BR>"
+	    );
+	    
+    	out.println ("<FORM ACTION =\"" + SMUtilities.getURLLinkBase(getServletContext()) + "smgl.GLCloseFiscalYearEdit\">");
+    	out.println("<INPUT TYPE=HIDDEN NAME='" + SMUtilities.SMCP_REQUEST_PARAM_DATABASE_ID + "' VALUE='" + sDBID + "'>\n");
+    	out.println("<INPUT TYPE=HIDDEN NAME='CallingClass' VALUE='" + this.getClass().getName() + "'>\n");
+
+		// Closable fiscal years:
+		//Get a drop down of the available periods:
+    	ArrayList<String> alValues = new ArrayList<String>(0);
+		ArrayList<String> alOptions = new ArrayList<String>(0);
+		alValues.clear();
+		alOptions.clear();
+
+		String sSQL = "SELECT"
+			+ " " + SMTableglfiscalperiods.ifiscalyear
+			+ " FROM " + SMTableglfiscalperiods.TableName
+			+ " WHERE ("
+				+ "(" + SMTableglfiscalperiods.ilockclosingperiod + " = 0)"
+			+ ")"
+			+ " ORDER BY " + SMTableglfiscalperiods.ifiscalyear
+		;
+		try {
+			ResultSet rsFiscalYears = clsDatabaseFunctions.openResultSet(
+				sSQL, 
+				getServletContext(), 
+				sDBID,
+				"MySQL",
+				this.toString() + ".getting fiscal year selections - User: " + sUserID
+				+ " - "
+				+ sUserFullName
+			);
+			while(rsFiscalYears.next()){
+				alValues.add(Long.toString(rsFiscalYears.getLong(SMTableglfiscalperiods.ifiscalyear)));
+				alOptions.add(Long.toString(rsFiscalYears.getLong(SMTableglfiscalperiods.ifiscalyear)));
+			}
+			rsFiscalYears.close();
+		} catch (Exception e1) {
+			out.println("<BR><FONT COLOR=RED><B>Error [1562701323] getting fiscal year selections - " + e1.getMessage() + "</B></FONT><BR>");
+		}
+		out.println("<BR>Close this fiscal year:&nbsp;");
+		
+		out.println("<SELECT NAME=\"" + PARAM_FISCAL_YEAR_SELECTION + "\"" 
+			+ " ID = \"" + 	PARAM_FISCAL_YEAR_SELECTION + "\""
+			+ "\">");
+		for (int i=0;i<alValues.size();i++){
+			out.println("<OPTION VALUE=\"" + alValues.get(i) + "\"> " + alOptions.get(i));
+		}
+		out.println("</SELECT>");
+		
+		out.println(
+    		"<BR>Set&nbsp;closing&nbsp;batch&nbsp;date&nbsp;to:&nbsp;"
+    		+ "<INPUT TYPE=TEXT NAME=\"" + PARAM_BATCH_DATE + "\""
+    		+ " VALUE=\"" + ServletUtilities.clsDateAndTimeConversions.now(ServletUtilities.clsServletUtilities.DATE_FORMAT_FOR_DISPLAY) + "\""
+    		+ " MAXLENGTH=" + "10"
+    		+ " SIZE = " + "8"
+    		+ ">"
+    		+ "\n"
+    	);
+		out.println(SMUtilities.getDatePickerString(PARAM_BATCH_DATE, getServletContext()) + "\n");
+    	
+    	out.println ("<BR><INPUT TYPE=\"SUBMIT\" VALUE=\"----Close year----\">");
+    	out.println("  Check to confirm process: <INPUT TYPE=CHECKBOX NAME=\"" + CONFIRM_PROCESS + "\"><BR>");
+    	out.println ("</FORM>");
+	    out.println("</BODY></HTML>");
+	}
 
 }

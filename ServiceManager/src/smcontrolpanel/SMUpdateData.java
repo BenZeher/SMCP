e6793814ca78d674e042a7157d602eec649fc9c2
ben <benalden@odcdc.com>
2019-10-07 07:59:50 -0400

    Drop scategorycode

--- ServiceManager/src/smcontrolpanel/SMUpdateData.java
+++ ServiceManager/src/smcontrolpanel/SMUpdateData.java
@@ -18,9 +18,9 @@ import ServletUtilities.clsDatabaseFunctions;
 
 public class SMUpdateData extends java.lang.Object{
 
-	private static final int m_CurrentDatabaseVersion = 1414;
+	private static final int m_CurrentDatabaseVersion = 1415;
 	private static final String m_sVersionNumber = "1.4";
-	private static final String m_sLastRevisionDate = "10/3/2019";
+	private static final String m_sLastRevisionDate = "10/7/2019";
 	private static final String m_sCopyright = "Copyright 2003-2019 AIRO Tech OMD, Inc.";
 
 	private String m_sErrorMessage;
@@ -14686,6 +14686,17 @@ public class SMUpdateData extends java.lang.Object{
 				//Added by BJA 9/27/2019
 				SQL = "ALTER TABLE `laborbackcharges` "
 					+ " ADD lcostcenterid int(11) NOT NULL DEFAULT '0'"
+				;
+				if (!execUpdate(sUser, SQL, conn, iSystemDatabaseVersion)){return false;}
+				iVersionUpdatedTo = iSystemDatabaseVersion + 1;
+			break;	
+			//END CASE
+			
+			//BEGIN CASE:
+			case 1414:
+				//Added by BJA 10/07/2019
+				SQL = "ALTER TABLE `laborbackcharges` "
+					+ " DROP  scategorycode"
 				;
 				if (!execUpdate(sUser, SQL, conn, iSystemDatabaseVersion)){return false;}
 				iVersionUpdatedTo = iSystemDatabaseVersion + 1;

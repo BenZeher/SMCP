86141a8575f753eb32c65219964186b6e5ae60c1
Tom Ronayne <tjprona@gmail.com>
2019-11-19 12:14:30 -0500

    Added glexternalcompanypulls.ipulltype

--- DataDefinitions/src/SMDataDefinition/SMTableglexternalcompanypulls.java
+++ DataDefinitions/src/SMDataDefinition/SMTableglexternalcompanypulls.java
@@ -13,11 +13,18 @@ public class SMTableglexternalcompanypulls {
 	public static final String luserid = "luserid";
 	public static final String sfullusername = "sfullusername";
 	public static final String ifiscalyear = "ifiscalyear";
-	public static final String ifiscalperiod = "ifiscalperiod"; 
+	public static final String ifiscalperiod = "ifiscalperiod";
+	public static final String ipulltype = "ipulltype";
 
 	//Field Lengths:
 	public static final int sdbnameLength = 64;
 	public static final int scompanynameLength = 128;
 	public static final int sfullusernameLength = 128;
+	
+	//Pull Types:
+	public static final int PULL_TYPE_PULL = 0;  //Normal pull type - when we pull transactions in from
+													// an external company
+	public static final int PULL_TYPE_REVERSE = 1; //This indicates the REVERSAL of a pull, such as when
+													// the user finds it necessary to 'roll back' a previous pull.
 }
 
--- ServiceManager/src/smcontrolpanel/SMUpdateData.java
+++ ServiceManager/src/smcontrolpanel/SMUpdateData.java
@@ -18,9 +18,9 @@ import ServletUtilities.clsDatabaseFunctions;
 
 public class SMUpdateData extends java.lang.Object{
 
-	private static final int m_CurrentDatabaseVersion = 1430;
+	private static final int m_CurrentDatabaseVersion = 1431;
 	private static final String m_sVersionNumber = "1.4";
-	private static final String m_sLastRevisionDate = "11/14/2019";
+	private static final String m_sLastRevisionDate = "11/19/2019";
 	private static final String m_sCopyright = "Copyright 2003-2019 AIRO Tech OMD, Inc.";
 
 	private String m_sErrorMessage;
@@ -14885,6 +14885,15 @@ public class SMUpdateData extends java.lang.Object{
 				SQL = "ALTER TABLE gltransactionlines ADD UNIQUE KEY `uniquetransactionkey`("
 					+ "loriginalbatchnumber, loriginalentrynumber, loriginallinenumber, ifiscalyear, ifiscalperiod, lexternalcompanypullid) ";
 				;
+				if (!execUpdate(sUser, SQL, conn, iSystemDatabaseVersion)){return false;}
+				iVersionUpdatedTo = iSystemDatabaseVersion + 1;
+			break;	
+			//END CASE
+			
+			//BEGIN CASE:
+			case 1430:
+				//Added by TJR 11/19/2019
+				SQL = "ALTER TABLE glexternalcompanypulls ADD COLUMN `ipulltype` INT(11) NOT NULL DEFAULT '0'";
 				if (!execUpdate(sUser, SQL, conn, iSystemDatabaseVersion)){return false;}
 				iVersionUpdatedTo = iSystemDatabaseVersion + 1;
 			break;	

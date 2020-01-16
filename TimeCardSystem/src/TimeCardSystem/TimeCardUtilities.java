e19459e0a8e06db373a58c44c1b3fedfcdfdc9db
Tom Ronayne <tjprona@gmail.com>
2020-01-16 17:35:55 -0500

    Updated TC revision date

--- TimeCardSystem/src/TimeCardSystem/TimeCardUtilities.java
+++ TimeCardSystem/src/TimeCardSystem/TimeCardUtilities.java
@@ -30,7 +30,7 @@ public class TimeCardUtilities extends clsServletUtilities{
 		"Transitional//EN\">";
 
 	public static final String sProgramVersion = "2.0";
-	public static final String sLastUpdated = "1/1/2020";
+	public static final String sLastUpdated = "1/16/2020";
 	public static final String sJob = "OHD";
 	public static final String sPro = "@#$";
 	//Session attributes

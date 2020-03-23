69efeb775dd18ef3ed7d4cfe68f93555e994fcff
Tom <tom@airotech.com>
2020-03-23 14:22:18 -0400

    Removed unneeded email function

--- ServletUtilities/src/ServletUtilities/clsServletUtilities.java
+++ ServletUtilities/src/ServletUtilities/clsServletUtilities.java
@@ -1200,77 +1200,7 @@ public class clsServletUtilities {
 		
 	}
 	
-	//The following function is only used in the 'Intranet'  project and the Time Card system:
-	public static void sendSSLMail(
-			String mailhost,
-			String sUserName,
-			String sPassword,
-			String smailport,
-			String subject, 
-			String body, 
-			String sender, 
-			String recipients,
-			boolean bUseHTML
-			) throws Exception 
-	{	
-		//Sample:
-		/*
-		SMUtilities.sendMail(
-			"smtp.gmail.com",
-			"tjprona@gmail.com",
-			"pw",
-			"465",
-			"Subject",
-			"This is a test message",
-			"test@guess.com",
-			"tom_ronayne@odcdc.com,tjprona@gmail.com");
-		
-		System.out.println("[1372257833] - "
-			+ "mailhost = '" + mailhost + "'\n"
-			+ ", sUserName = '" + sUserName + "'\n"
-			+ ", sPassword = '" + sPassword + "'\n"
-			+ ", smailport = '" + smailport + "'\n"
-			+ ", subject = '" + subject + "'\n"
-			+ ", body = '" + body + "'\n"
-			+ ", sender = '" + sender + "'\n"
-			+ ", recipients = '" + recipients + "'\n"
-		);
-		*/
-		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-		final String sUser = sUserName;
-		final String sPw = sPassword;
-		Properties props = new Properties();
-		props.setProperty("mail.transport.protocol", "smtp");
-		props.setProperty("mail.host", mailhost);
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", smailport);
-		props.put("mail.smtp.socketFactory.port", smailport);
-		props.put("mail.smtp.socketFactory.class",
-		"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.socketFactory.fallback", "false");
-		props.setProperty("mail.smtp.quitwait", "false");
-		Session session = Session.getDefaultInstance(props,
-				new javax.mail.Authenticator() 
-		{
-			protected PasswordAuthentication getPasswordAuthentication()
-			{ return new PasswordAuthentication(sUser,sPw);	}
-		});		
-		MimeMessage message = new MimeMessage(session);
-		message.setSender(new InternetAddress(sUserName));
-		message.setSubject(subject);
-		if (bUseHTML){
-			message.setContent(body, "text/html");
-		}else{
-			message.setContent(body, "text/plain");
-		}
-		if (recipients.indexOf(',') > 0){ 
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
-		}else{
-			message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
-		}
-		
-		Transport.send(message);
-	}
+
 	//This function is only called from within this class:
     private static void sendEmailWithEmbeddedHTML(
     		String host,

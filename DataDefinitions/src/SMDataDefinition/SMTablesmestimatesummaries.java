32deb009b012f8a306eddb0f1527c21efeee259e
Tom <tom@airotech.com>
2020-05-19 15:41:46 -0400

    Started adding estimate tables

---
+++ DataDefinitions/src/SMDataDefinition/SMTablesmestimatesummaries.java
@@ -0,0 +1,36 @@
+package SMDataDefinition;
+
+public class SMTablesmestimatesummaries {
+
+	public static String TableName = "smestimatesummaries";
+
+	public static String lid = "lid";
+	public static String sjobname = "sjobname";
+	public static String lsalesleadid = "lsalesleadid";
+	public static String itaxid = "itaxid";
+	public static String ilabortype = "ilabortype";
+	public static String iordertype = "iordertype";
+	public static String lcreatedbyid = "lcreatedbyid";
+	public static String datetimecreated = "datetimecreated";
+	public static String screatedbyfullname = "screatedbyfullname";
+	public static String llastmodifiedbyid = "llastmodifiedbyid";
+	public static String datetimeslastmodifiedby = "datetimeslastmodifiedby";
+	public static String slastmodifiedbyfullname = "slastmodifiedbyfullname";
+	public static String sdescription = "sdescription";
+	public static String sremarks = "sremarks";
+	public static String bdadjustedfreight = "bdadjustedfreight";
+	public static String bdadjustedlaborunitqty = "bdadjustedlaborunitqty";
+	public static String bdadjustedlaborcostperunit = "bdadjustedlaborcostperunit";
+	public static String bdadjustedlmarkupamt = "bdadjustedlmarkupamt";
+	
+	public static int sjobnameLength = 128;
+	public static int sscreatedbyfullnameLength = 128;
+	public static int slastmodifiedbyfullnameLength = 128;
+	public static int sdescriptionLength = 128;
+	public static int sremarksLength = 128;
+	public static int bdadjustedfreightScale = 2;
+	public static int bdadjustedlaborunitqtyScale = 4;
+	public static int bdadjustedlaborcostperunitScale = 2;
+	public static int bdadjustedlmarkupamtScale = 2;
+	
+}

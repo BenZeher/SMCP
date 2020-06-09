4d6e02db00d1af37ae94595e8445785d760dffc9
ben <benalden@odcdc.com>
2020-06-09 10:51:34 -0400

    Unused Imports

--- ServiceManager/src/smic/ICPOHeader.java
+++ ServiceManager/src/smic/ICPOHeader.java
@@ -5,8 +5,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Enumeration;
 
 import javax.servlet.ServletContext;
